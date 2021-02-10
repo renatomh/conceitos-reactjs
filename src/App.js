4b9c2a1b91c3a02e1f284495cb21aab7663d44cd
Renato Marino Henz <renatomarinohenz@gmail.com>
2021-02-10 16:12:36 -0300

    Resolvido o desafio, implementando as funcionalidades requeridas para as chamadas à API.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  // Criando o estado para os repositórios
+  const [repositories, setRepositories] = useState([]);
+
+  // Atualizando a lista de repositórios
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, []);
+
+  // Cadastrando um novo repositório
   async function handleAddRepository() {
-    // TODO
+    // Fazendo o cadastrado com a chamada à API
+    const response = await api.post('repositories', {
+      title: 'Umbriel',
+      url: 'https://github.com/rocketseat/umbriel',
+      techs: ['Node.js', 'ReactJS'],
+    })
+    // Salvando o resultado obtido no estado criado
+    const repository = response.data;
+    setRepositories([...repositories, repository])
   }
 
+  // Removendo um repositório
   async function handleRemoveRepository(id) {
-    // TODO
+    // Fazendo a chamada à API
+    await api.delete(`repositories/${id}`)
+
+    // Atualizando a lista de repositórios no estado
+    setRepositories(repositories.filter(
+      repository => repository.id != id
+    ))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
           </button>
-        </li>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
